f5504ffb716427f123c70d5aa3bafb3bd70fee29
MatwejVasilchenko <matwejkotofej@gmail.com>
2023-06-22 18:29:19 +0300

    рррр

--- 11JSHM6.js
+++ 11JSHM6.js
@@ -1,1 +1,16 @@
-//dopomoga
+let input;
+const numbers = [];
+let total = 0;
+
+do {
+  input = prompt('Введите число:');
+  if (input !== null && input !== '') {
+    numbers.push(Number(input));
+  }
+} while (input !== null);
+
+for (const number of numbers) {
+  total += number;
+}
+
+console.log(`Общая сумма чисел равна ${total}`);
--- 12JSHM5.js
+++ 12JSHM5.js
@@ -1,1 +1,12 @@
-//допомога
+const buttonRef = document.querySelector(".fifeAnswerButtonJS");
+
+
+
+buttonRef.addEventListener("click", clicksButton);
+
+const clicksButton = () => {
+    let clicks = 0;
+    clicks += 1;
+    console.log(clicks);
+    return clicks;
+}
